367da7362319c3d64e08bb4c2412c11851b0f84a
lucky masharane <lmasharane@gmail.com>
2021-01-25 13:45:21 +0200

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDcqdJpeGWoGeekp3my7mOFWuxe_6uWWnw",
+    authDomain: "news-app-7345c.firebaseapp.com",
+    projectId: "news-app-7345c",
+    storageBucket: "news-app-7345c.appspot.com",
+    messagingSenderId: "112277384100",
+    appId: "1:112277384100:web:b8f1484fef5efc59242f31"
+  }
 };
